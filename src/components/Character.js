ec8d8d07f7cf03d2f90b7735f563deff9897f85b
Sean-Ubele <seanjohnreedubeleknuckles@gmail.com>
2022-03-25 18:51:36 -0400

    mvp

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,43 @@
 // Write your Character component here
+import React from "react";
+import styled  from "styled-components";
+
+const StyledCharacter = styled.div `
+    display: flex;
+    flex-flow: row-wrap;
+    align-items: center;
+    border: solid white 1px;
+    margin: 0px auto;
+    margin-top: 50px;
+    padding: 50px;
+    max-width: 60%;
+    
+`;
+const StyledHeading = styled.h3`
+    display: flex;
+    flex-flow: row-wrap;
+    justify-content: center;
+`;
+
+const Character = (props) => {
+    return (
+        <>
+        {props.characterList.map((character, id) => {
+            return (
+                <StyledCharacter key={id}>
+                    <StyledHeading>Name: {character.name}</StyledHeading>
+                    <p>Year Born: {character.birth_year} </p>
+                    <p>Height: {character.height} </p>
+                    <p>Mass: {character.mass} </p>
+                    <p>Skin Color: {character.skin_color} </p>
+                    <p>Hair Color: {character.hair_color} </p>
+                    <p>Eye Color: {character.eye_color} </p>
+                </StyledCharacter>
+
+            )
+        })}
+        </>
+    )
+}
+
+export default Character
